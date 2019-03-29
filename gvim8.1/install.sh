9c441d682b953adec58004b615cc91cecc714479
Hello_U <LarT2P@users.noreply.github.com>
2019-03-29 15:51:54 +0800

    Create install.sh

---
+++ gvim8.1/install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+BASEDIR=$(dirname $0)
+cd $BASEDIR
+CURRENT_DIR=`pwd`
+
+# 定义链接函数
+lnif() {
+    if [ -e "$1" ]; then
+        ln -sf "$1" "$2"
+    fi
+}
+
+echo "Step1: backing up current vim config"
+today=`date +%Y%m%d`
+for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc; do [ -e $i ] && [ ! -L $i ] && mv $i $i.$today; done
+for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc; do [ -L $i ] && unlink $i ; done
+
+echo "Step2: setting up symlinks"
+lnif $CURRENT_DIR/vimrc $HOME/.vimrc
+lnif $CURRENT_DIR/vim/ $HOME/.vim
+
+echo "Step3: update/install plugins using Vim-plug"
+system_shell=$SHELL
+export SHELL="/bin/sh"
+vim -u $HOME/.vimrc +PlugInstall! +PlugClean! +qall
+export SHELL=$system_shell
+
+echo "Install Done!"
